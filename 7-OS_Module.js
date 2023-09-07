f6ee564ed9fa1561a6c6d7c8b238da87caf05112
rajdeep298 <rajdeepkundu2983@gmail.com>
2023-09-07 11:58:51 +0530

    some mild changes

--- 7-OS_Module.js
+++ 7-OS_Module.js
@@ -4,7 +4,7 @@ const os =require('os');
 const user = os.userInfo();
 console.log(user);
 
-console.log("The system uptime is "+os.uptime()+" seconds");
+console.log("The system's uptime is "+os.uptime()+" seconds");
 
 const currentOS = {
     name:os.type(),
